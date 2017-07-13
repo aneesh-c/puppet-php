bde11d9fbeef6b8e3cbe008bcdf8906f7f69383f
Aneesh C <admn.nanonet@gmail.com>
2017-07-13 12:39:27 +0530

    Release 5.0.0

--- manifests/params.pp
+++ manifests/params.pp
@@ -9,46 +9,68 @@ class php::params inherits php::version {
     $configfile_wwwconf = '/etc/php-fpm.d/www.conf'
   }
   elsif $::osfamily == 'Debian' and $ver == undef {
-    case $::operatingsystemrelease {
-      /^12.*/: {
-        $packagename_mod = [ 'php5-mcrypt', 'php5-gd', 'libapache2-mod-php5' ]
-        $package_name = [ 'php5' ]
-        $package_name_fpm = [ 'php5-fpm' ]
-        $configfile = '/etc/php5/apache2/php.ini'
-        $configfile_fpm = '/etc/php5/fpm/php.ini'
-        $configfile_wwwconf = '/etc/php5/fpm/pool.d/www.conf'
+    if $::operatingsystem == 'Ubuntu' {
+      case $::operatingsystemrelease {
+        /^12.*/: {
+          $packagename_mod = [ 'php5-mcrypt', 'php5-gd', 'libapache2-mod-php5' ]
+          $package_name = [ 'php5' ]
+          $package_name_fpm = [ 'php5-fpm' ]
+          $configfile = '/etc/php5/apache2/php.ini'
+          $configfile_fpm = '/etc/php5/fpm/php.ini'
+          $configfile_wwwconf = '/etc/php5/fpm/pool.d/www.conf'
+        }
+        /^14.*/: {
+          $packagename_mod = [ 'php5-mcrypt', 'php5-gd', 'libapache2-mod-php5' ]
+          $package_name = [ 'php5' ]
+          $package_name_fpm = [ 'php5-fpm' ]
+          $configfile = '/etc/php5/apache2/php.ini'
+          $configfile_fpm = '/etc/php5/fpm/php.ini'
+          $configfile_wwwconf = '/etc/php5/fpm/pool.d/www.conf'
+        }
+        /^16.*/: {
+          $packagename_mod = [ 'php-mcrypt', 'php7.0-gd', 'libapache2-mod-php' ]
+          $package_name = [ 'php' ]
+          $package_name_fpm = [ 'php-fpm' ]
+          $configfile = '/etc/php/7.0/apache2/php.ini'
+          $configfile_fpm = '/etc/php/7.0/fpm/php.ini'
+          $configfile_wwwconf = '/etc/php/7.0/fpm/pool.d/www.conf'
+        }
+        /^17.*/: {
+          $packagename_mod = [ 'php-mcrypt', 'php7.0-gd', 'libapache2-mod-php' ]
+          $package_name = [ 'php' ]
+          $package_name_fpm = [ 'php-fpm' ]
+          $configfile = '/etc/php/7.0/apache2/php.ini'
+          $configfile_fpm = '/etc/php/7.0/fpm/php.ini'
+          $configfile_wwwconf = '/etc/php/7.0/fpm/pool.d/www.conf'
+        }
+        default: {
+          $packagename_mod = [ 'php5-mcrypt', 'php5-gd', 'libapache2-mod-php5' ]
+          $package_name = [ 'php5' ]
+          $package_name_fpm = [ 'php5-fpm' ]
+          $configfile = '/etc/php5/apache2/php.ini'
+          $configfile_fpm = '/etc/php5/fpm/php.ini'
+          $configfile_wwwconf = '/etc/php5/fpm/pool.d/www.conf'
+        }
       }
-      /^14.*/: {
-        $packagename_mod = [ 'php5-mcrypt', 'php5-gd', 'libapache2-mod-php5' ]
-        $package_name = [ 'php5' ]
-        $package_name_fpm = [ 'php5-fpm' ]
-        $configfile = '/etc/php5/apache2/php.ini'
-        $configfile_fpm = '/etc/php5/fpm/php.ini'
-        $configfile_wwwconf = '/etc/php5/fpm/pool.d/www.conf'
-      }
-      /^16.*/: {
-        $packagename_mod = [ 'php-mcrypt', 'php7.0-gd', 'libapache2-mod-php' ]
-        $package_name = [ 'php' ]
-        $package_name_fpm = [ 'php-fpm' ]
-        $configfile = '/etc/php/7.0/apache2/php.ini'
-        $configfile_fpm = '/etc/php/7.0/fpm/php.ini'
-        $configfile_wwwconf = '/etc/php/7.0/fpm/pool.d/www.conf'
-      }
-      /^17.*/: {
-        $packagename_mod = [ 'php-mcrypt', 'php7.0-gd', 'libapache2-mod-php' ]
-        $package_name = [ 'php' ]
-        $package_name_fpm = [ 'php-fpm' ]
-        $configfile = '/etc/php/7.0/apache2/php.ini'
-        $configfile_fpm = '/etc/php/7.0/fpm/php.ini'
-        $configfile_wwwconf = '/etc/php/7.0/fpm/pool.d/www.conf'
-      }
-      default: {
-        $packagename_mod = [ 'php5-mcrypt', 'php5-gd', 'libapache2-mod-php5' ]
-        $package_name = [ 'php5' ]
-        $package_name_fpm = [ 'php5-fpm' ]
-        $configfile = '/etc/php5/apache2/php.ini'
-        $configfile_fpm = '/etc/php5/fpm/php.ini'
-        $configfile_wwwconf = '/etc/php5/fpm/pool.d/www.conf'
+    }
+    elsif $::operatingsystem == 'Debian' {
+      case $::operatingsystemrelease {
+        /^9.*/: {
+          $packagename_mod = [ 'php-mcrypt', 'php7.0-gd', 'libapache2-mod-php' ]
+          $package_name = [ 'php' ]
+          $package_name_fpm = [ 'php-fpm' ]
+          $configfile = '/etc/php/7.0/apache2/php.ini'
+          $configfile_fpm = '/etc/php/7.0/fpm/php.ini'
+          $configfile_wwwconf = '/etc/php/7.0/fpm/pool.d/www.conf'
+        }
+        default: {
+          $packagename_mod = [ 'php5-mcrypt', 'php5-gd', 'libapache2-mod-php5' ]
+          $package_name = [ 'php5' ]
+          $package_name_fpm = [ 'php5-fpm' ]
+          $configfile = '/etc/php5/apache2/php.ini'
+          $configfile_fpm = '/etc/php5/fpm/php.ini'
+          $configfile_wwwconf = '/etc/php5/fpm/pool.d/www.conf'
+        }
       }
     }
   }
