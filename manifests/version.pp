e2cef936768068572faec55353e89f18260cc9ed
Aneesh C <admn.nanonet@gmail.com>
2017-08-15 09:50:22 +0530

    Release 5.0.2

--- manifests/version.pp
+++ manifests/version.pp
@@ -10,6 +10,14 @@ class php::version (
       $configfile_fpm = '/etc/php/7.1/fpm/php.ini'
       $configfile_wwwconf = '/etc/php/7.1/fpm/pool.d/www.conf'
     }
+    elsif $php_version == '5.6' {
+      $ver = true
+      $package_name = [ 'php5.6', 'libapache2-mod-php5.6' ]
+      $package_name_fpm = [ 'php5.6-fpm' ]
+      $configfile = '/etc/php/5.6/apache2/php.ini'
+      $configfile_fpm = '/etc/php/5.6/fpm/php.ini'
+      $configfile_wwwconf = '/etc/php/5.6/fpm/pool.d/www.conf'
+    }
     else {
       $ver = undef
     }
