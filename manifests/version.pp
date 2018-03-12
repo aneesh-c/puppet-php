94fbde7bf962e431027dbc3d155281dbb6461e60
Aneesh C <admn.nanonet@gmail.com>
2018-03-12 16:46:12 +0530

    Release 5.0.3

--- manifests/version.pp
+++ manifests/version.pp
@@ -2,7 +2,15 @@ class php::version (
   $php_version = undef,
 ){
   if $::osfamily == 'Debian' {
-    if $php_version == '7.1' {
+    if $php_version == '7.2' {
+      $ver = true
+      $package_name = [ 'php7.2', 'libapache2-mod-php7.2' ]
+      $package_name_fpm = [ 'php7.2-fpm' ]
+      $configfile = '/etc/php/7.2/apache2/php.ini'
+      $configfile_fpm = '/etc/php/7.2/fpm/php.ini'
+      $configfile_wwwconf = '/etc/php/7.2/fpm/pool.d/www.conf'
+    }
+    elsif $php_version == '7.1' {
       $ver = true
       $package_name = [ 'php7.1', 'libapache2-mod-php7.1' ]
       $package_name_fpm = [ 'php7.1-fpm' ]
